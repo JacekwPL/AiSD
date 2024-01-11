f0662a99bd3b60fec1df911bb32e604400d0ca9c
JacekwPL <38696493+JacekwPL@users.noreply.github.com>
2024-01-12 00:01:01 +0100

    poprawa czytelności kodu

--- StarLogic.py
+++ StarLogic.py
@@ -4,13 +4,13 @@ class Star:
         self.__pos = position
 
     def __str__(self):
-        return f'{self.__name} {self.pos}'
+        return f'{self.__name} {self.__pos}'
 
     def getName(self) -> str:
         return self.__name
 
-    def setName(self, newName: str):
-        self.__name = newName
+    def setName(self, name: str):
+        self.__name = name
 
     def getPosition(self):
         return self.__pos
@@ -22,6 +22,16 @@ class Star:
 class ListOfStars:
     def __init__(self):
         self.Lista = {}
+
+    def __len__(self):
+        return len(self.Lista)
+
+    def __str__(self) -> str:
+        xpp = ""
+        for key in self.Lista.keys():
+            xpp += f'Nazwa gwiazdy: {key}, pozycja: {self.Lista[key]}'
+
+        return xpp
 
     def importFromFile(self, path):
         pass
@@ -49,14 +59,6 @@ class ListOfStars:
             print(str(self), file=f)
 
             f.close()
-
-    def __len__(self):
-        return len(self.Lista)
-
-    def __str__(self) -> str:
-        xpp = ""
-        for key in self.Lista.keys():
-            xpp += f'Nazwa gwiazdy: {key}, pozycja: {self.Lista[key]}'
 
 
 if __name__ == '__main__':
