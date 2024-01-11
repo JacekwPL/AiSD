868c94a6f6b03c9e66fe3a37cf3206daaec89d9c
JacekwPL <38696493+JacekwPL@users.noreply.github.com>
2024-01-11 23:54:13 +0100

    poprawa czytelności kodu
    Przeniesienie wbudowanych nadpisanych metod na górę klasy

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-import StarLogic
+from StarLogic import Star, ListOfStars
+
+if __name__ == '__main__':
+    while True:
+        operacja = input('prompt')
+
+        match operacja:
+            case '1':
+                pass
+
+        break
